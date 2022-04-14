2690400aae322e7c7bc597b5e8dc31e8a97b9c65
libbykoolik <94492257+lkoolik@users.noreply.github.com>
2022-04-14 14:54:46 -0700

    Add isrm-env, requirements, and upgrade console based functionality

--- isrm_calcs.py
+++ isrm_calcs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/home/usr/virtualenv/env python3
 # -*- coding: utf-8 -*-
 """
 Main Run File
@@ -8,13 +8,13 @@ Main Run File
 #%% Import useful libraries
 from pathlib import Path
 import sys
-sys.path.append('/Users/libbykoolik/Documents/Research/OEHHA Project/scripts/isrm_health_calculations/supporting')
+sys.path.insert(0,'./supporting')
 from isrm import isrm
 from emissions import emissions
-
-#%% Import the argparse library to parse command line arguments
+from concentration import concentration
 import argparse
 
+#%% Use argparse to parse command line arguments
 # Initialize the parser object
 parser = argparse.ArgumentParser(description="Runs the ISRM-based tool for estimating PM2.5 concentrations and associated health impacts.")
 
@@ -24,15 +24,21 @@ parser.add_argument("-c", "--checkinputs", help="use this run option if you want
 parser.add_argument("-e", "--emissions", help="emissions input file path", type=str)
 parser.add_argument("-u", "--units", help="emissions units")
 parser.add_argument("-sn", "--scenarioname", help="name of scenario being modeled", type=str)
+parser.add_argument("-v", "--verbose", help="include this tag to run the code in verbose mode", action='store_true')
 
 # Parse all arguments
 args = parser.parse_args()
 
 # Separate arguments into useful variables
 check = args.checkinputs
+verbose = args.verbose
 if args.emissions: emissions_path = args.emissions
-if args.units: units = args.units
-if args.scenarioname: name = args.scenarioname
+units = args.units if args.units else 'ug/s'
+name = args.scenarioname if args.scenarioname else ''
+
+# Define ISRM Variables
+isrm_fp = './data/ca_isrm.ncf'
+isrm_gfp = './data/InMAP_gridCells.shp'
 
 #%% Run Program
 if __name__ == "__main__":
@@ -40,5 +46,19 @@ if __name__ == "__main__":
     # If check module is selected, run a file check and then exit without 
     # running calculations
     if check:
-        emis = emissions(emissions_path, load_file=False, verbose=True)
+        try:
+            # Default to verbose since this mode is just for checking files
+            emis = emissions(emissions_path, units=units, name=name, load_file=False, verbose=True)
+            isrmgrid = isrm(isrm_fp, isrm_gfp, load_file=False, verbose=True)
+            print("\n<< Emissions and ISRM file exist and are able to be imported. >>\n")
+        except:
+            print("\n<< Correct error messages above before running the program. >>\n")
+        quit()
+    else: # for now, run concentration calculations since no health built
+        emis = emissions(emissions_path, units=units, name=name, load_file=True, verbose=verbose)
+        isrmgrid = isrm(isrm_fp, isrm_gfp, load_file=True, verbose=verbose)
+        conc = concentration(emis, isrmgrid, run_calcs=True, verbose=verbose)
+        
+        print("\n<< Concentrations estimated >>\n")
+        quit()
         
